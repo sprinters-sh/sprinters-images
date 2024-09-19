019b67a1bed949d38fc0e2af87ae80d1d5c9ea23
axel <684824+axelfontaine@users.noreply.github.com>
2024-09-19 10:38:12 +0200

    Added dedicated entrypoint.sh to work around race condition between dockerd and runner startup

---
+++ entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+
+# Function to start a process and wait for a specific message
+# Arguments:
+# 1. Command to run the process
+# 2. Specific message to wait for
+wait_for_message_and_background() {
+    local process_command="$1"
+    local success_message="$2"
+    local failure_message="$3"
+    local log=background_output.log
+
+    echo "Starting '$process_command' ..."
+
+    # Start the process in the background
+    $process_command > >(tee $log) 2>&1 &
+
+    # Capture the PID of the process
+    local pid=$!
+
+    # Ensure we can read the log even if the process_command included sudo
+    chmod +r $log
+
+    # Use tail -f to monitor the output and look for the specific message
+    tail -f $log | while read -r line; do
+        # Check for the specific message
+        if echo "$line" | grep -q "$success_message"; then
+            echo "Success message detected: '$success_message'."
+            break
+        fi
+
+        # Check for the failure message
+        if echo "$line" | grep -q "$failure_message"; then
+            echo "Failure message detected: '$failure_message'. Exiting with failure."
+            exit 1
+        fi
+    done || exit 1
+
+    # If the process is still running (detached), move it to the background
+    echo "Successfully started '$process_command'. Moving process to the background."
+    disown $pid
+}
+
+wait_for_message_and_background "sudo dockerd" "API listen on /var/run/docker.sock" "exit status"
+./config.sh --url https://github.com/$REPO \
+            --token $TOKEN \
+            --labels $LABELS \
+            --ephemeral --disableupdate --unattended \
+  && ./run.sh
--- minimal.cmd
+++ minimal.cmd
@@ -1,1 +1,2 @@
 docker build -t sprinter-image-minimal:dev -f Dockerfile-minimal .
+docker run --privileged --rm sprinter-image-minimal:dev
