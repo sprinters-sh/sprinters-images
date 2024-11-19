55f4b18eff1813e27aeae361c96e5c0a2dfe7564
axel <684824+axelfontaine@users.noreply.github.com>
2024-11-19 09:15:22 +0100

    Updated runner versions

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,53 +1,63 @@
 #!/bin/bash
 
-# Function to start a process and wait for a specific message
+# Prefix text with timestamp (Example: 2024-11-19 08:08:03 My command output)
+timestamp() {
+    echo "$(date -u -Iseconds | tr 'T' ' ' | cut -d'+' -f1)" "$@"
+}
+
+# Run command with optional output suppression
+run_quiet() {
+    if [ -n "${QUIET}" ]; then
+        "$@" >/dev/null 2>&1
+    else
+        "$@"
+    fi
+}
+
+# Start a process and wait for a specific message
 # Arguments:
-# 1. Command to run the process
-# 2. Specific message to wait for
+# 1. Log file for output
+# 2. Command to run the process
+# 3. Regex indicating success when matched in output
+# 3. Regex indicating failure when matched in output
 wait_for_message_and_background() {
     local process_log="$1"
     local process_command="$2"
-    local success_message="$3"
-    local failure_expr="$4"
+    local success_regex="$3"
+    local failure_regex="$4"
 
-    echo "Starting '$process_command' ..."
-
-    # Precreate log file and ensure it exists with readable permissions
+    timestamp "Starting '$process_command' ..."
     touch "$process_log"
 
-    # Start the process in the background
-    $process_command > >(tee "$process_log") 2>&1 &
-
-    # Capture the PID of the process
+    # Start process with timestamp
+    (timestamp "Process starting"; $process_command) > >(run_quiet tee "$process_log") 2>&1 &
     local pid=$!
 
-    # Use tail -f to monitor the output and look for the specific message
-    tail -s 0.1 -f "$process_log" | while read -r line; do
-        # Check for the specific message
-        if echo "$line" | grep -q "$success_message"; then
+    timestamp "Waiting for success message..."
+    while true; do
+        if grep -q "$success_regex" "$process_log"; then
             break
         fi
-
-        # Check for the failure message
-        if echo "$line" | grep -q "$failure_expr"; then
+        if grep -q "$failure_regex" "$process_log"; then
             echo "Failure message detected. Exiting with failure."
             exit 1
         fi
-    done || exit 1
+        sleep 0.1
+    done
 
     # If the process is still running (detached), move it to the background
-    echo "Successfully started '$process_command' (pid: $pid) -> moving process to the background."
+    timestamp "Successfully started '$process_command' (pid: $pid) -> moving process to the background."
     disown $pid
 }
 
 wait_for_message_and_background "containerd.log" "sudo containerd" "containerd successfully booted" "failed to start containerd"
 wait_for_message_and_background "dockerd.log" "sudo dockerd -D --containerd /run/containerd/containerd.sock" "API listen on /var/run/docker.sock" "exit status\|failed to start containerd"
 
-echo "Configuring Runner ..."
-./config.sh --url https://github.com/$REPO \
+timestamp "Configuring Runner ..."
+(run_quiet ./config.sh --url https://github.com/$REPO \
             --token $TOKEN \
             --labels $LABELS \
-            --ephemeral --disableupdate --unattended || exit 1
+            --ephemeral --disableupdate --unattended) || exit 1
 
-echo "Launching Runner ..."
-./run.sh
+timestamp "Launching Runner ..."
+run_quiet ./run.sh
