586f8671f3f28bdb95c550ff33b4cb31a814c8d6
axel <684824+axelfontaine@users.noreply.github.com>
2025-02-24 20:54:47 +0100

    Updated to 20250223 Ubuntu images

--- job-started.sh
+++ job-started.sh
@@ -5,14 +5,15 @@ timestamp() {
     echo "$(date -u -Iseconds | tr 'T' ' ' | cut -d'+' -f1)" "$@"
 }
 
-timestamp "===> Sprinters Job Starting..."
+timestamp "Sprinters Job Starting..."
 
 until docker info >/dev/null 2>&1; do
     timestamp "Waiting for Docker daemon to be ready..."
     sleep 1
 done
 timestamp "Docker daemon is ready!"
-
+timestamp "Sprinters Job Started."
+echo ""
 timestamp "Instance: $(hostname)"
-
-timestamp "===> Sprinters Job Started."
+timestamp "vCPUs   : $(nproc)"
+timestamp "RAM     : $(grep MemTotal /proc/meminfo | awk '{printf "%d", $2/1024/1024}') GiB"
