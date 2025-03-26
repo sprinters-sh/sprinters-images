7cb435cf6ec344036c204a2cd6fc65ab241b4dfe
axel <684824+axelfontaine@users.noreply.github.com>
2025-03-26 16:12:42 +0100

    Fine-tuned runner info output

--- job-completed.sh
+++ job-completed.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-echo "Temp Usage: $(df | grep /dev | head -1  | awk '{print $5}')"
+echo "Temp usage : $(df | grep /dev | head -1  | awk '{print $5}')"
 
 /publish-event.sh job-completed
--- job-started.sh
+++ job-started.sh
@@ -6,12 +6,12 @@ until docker info >/dev/null 2>&1; do
     echo "Waiting for Docker daemon to be ready..."
     sleep 1
 done
+echo
 echo "Runner powered by Sprinters"
+echo "---------------------------"
 echo "Instance : $(hostname)"
 echo "vCPUs    : $(nproc)"
 echo "RAM      : $(grep MemTotal /proc/meminfo | awk '{value=$2/1024/1024; if(value!=int(value)) value=int(value)+1; print value}') GiB"
-echo "Root     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep -v -e SWAP -e /var | awk '{print $1/1024^3}') GiB"
+echo "Root     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep -v -e SWAP -e / | awk '{print $1/1024^3}') GiB"
 echo "Swap     : $(lsblk -n -d -o SIZE,MOUNTPOINTS --bytes | grep SWAP | awk '{print $1/1024^3}') GiB"
-echo "Temp     : $(lsblk -n -d -o SIZE,MOUNTPOINTS --bytes | grep disk | grep /var | awk '{print $1/1024^3}') GiB"
-echo "Disks    :"
-lsblk --bytes -d -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINTS
+echo "Temp     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep / | awk '{print $1/1024^3}') GiB"
