138b5e7e78d23691d684978e93adf91425fe8589
axel <684824+axelfontaine@users.noreply.github.com>
2025-03-26 14:13:55 +0100

    Fine-tuned runner info output

--- job-started.sh
+++ job-started.sh
@@ -10,6 +10,8 @@ echo "Runner powered by Sprinters"
 echo "Instance : $(hostname)"
 echo "vCPUs    : $(nproc)"
 echo "RAM      : $(grep MemTotal /proc/meminfo | awk '{value=$2/1024/1024; if(value!=int(value)) value=int(value)+1; print value}') GiB"
-echo "Swap     : $(lsblk -n -d -o NAME,SIZE,FSTYPE --bytes | grep swap | awk '{print $2/1024^3}') GiB"
+echo "Root     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep -v -e SWAP -e /var | awk '{print $1/1024^3}') GiB"
+echo "Swap     : $(lsblk -n -d -o SIZE,MOUNTPOINTS --bytes | grep SWAP | awk '{print $1/1024^3}') GiB"
+echo "Temp     : $(lsblk -n -d -o SIZE,MOUNTPOINTS --bytes | grep disk | grep /var | awk '{print $1/1024^3}') GiB"
 echo "Disks    :"
-lsblk -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINTS
+lsblk --bytes -d -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINTS
