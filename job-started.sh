55ab8ed6d20220052c1f1d2db7e5bd434e666887
axel <684824+axelfontaine@users.noreply.github.com>
2025-03-26 12:42:24 +0100

    Fine-tuned runner info output

--- job-started.sh
+++ job-started.sh
@@ -6,10 +6,10 @@ until docker info >/dev/null 2>&1; do
     echo "Waiting for Docker daemon to be ready..."
     sleep 1
 done
-echo ""
+echo "Runner powered by Sprinters"
 echo "Instance : $(hostname)"
 echo "vCPUs    : $(nproc)"
 echo "RAM      : $(grep MemTotal /proc/meminfo | awk '{value=$2/1024/1024; if(value!=int(value)) value=int(value)+1; print value}') GiB"
 echo "Swap     : $(lsblk -n -d -o NAME,SIZE,FSTYPE --bytes | grep swap | awk '{print $2/1024^3}') GiB"
 echo "Disks    :"
-lsblk -n -d -o NAME,SIZE,FSTYPE --bytes | awk '{printf "  %s (%s)\t%.0f GiB\n", $1, $3, $2/(1024^3)}'
+lsblk -o NAME,SIZE,TYPE,FSTYPE,MOUNTPOINTS
