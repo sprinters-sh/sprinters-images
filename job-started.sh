7e2dd06203da1b4f757f1b9cd52f1d2131f88cb5
axel <684824+axelfontaine@users.noreply.github.com>
2025-04-07 16:17:52 +0200

    Show env vars

--- job-started.sh
+++ job-started.sh
@@ -9,9 +9,14 @@ done
 echo
 echo "Runner powered by Sprinters"
 echo "---------------------------"
+echo "Name     : $RUNNER_NAME"
 echo "Instance : $(hostname)"
 echo "vCPUs    : $(nproc)"
 echo "RAM      : $(sudo lshw -short -C memory | grep -i "system memory" | awk '{print $3}' | sed 's/GiB//') GiB"
 echo "Root     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep -v -e SWAP -e / | awk '{print $1/1024^3}') GiB"
 echo "Swap     : $(lsblk -n -d -o SIZE,MOUNTPOINTS --bytes | grep SWAP | awk '{print $1/1024^3}') GiB"
 echo "Temp     : $(lsblk -n -d -o SIZE,MOUNTPOINTS,TYPE --bytes | grep disk | grep / | awk '{print $1/1024^3}') GiB"
+echo
+echo "Environment Variables"
+echo "---------------------"
+env | sort
