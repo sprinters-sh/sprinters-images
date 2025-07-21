d85b2a6155799a9444a12594593c05d54cd35a06
axel <684824+axelfontaine@users.noreply.github.com>
2025-07-21 13:00:29 +0200

    Updated images

--- images/common/publish-event.sh
+++ images/common/publish-event.sh
@@ -1,17 +1,41 @@
 #!/bin/bash
+# shellcheck disable=SC2155
 
-url="$SPRINTERS_RUNNER_WEBHOOK"
+readonly url="$SPRINTERS_RUNNER_WEBHOOK"
 if [ -z "$url" ]; then
   # Event publishing is disabled
   exit 0
 fi
 
-event=$1
-hostname=$(hostname)
-timestamp="$(date -u -Ins  | tr ',' '.' | cut -c1-23)Z"
+get_env_value_string() {
+  local var_name="$1"
+  if [[ -n "${!var_name+x}" ]]; then
+    echo "\"${!var_name}\""
+  else
+    echo "null"
+  fi
+}
+
+get_env_value_number() {
+  local var_name="$1"
+  if [[ -n "${!var_name+x}" ]]; then
+    echo "${!var_name}"
+  else
+    echo "null"
+  fi
+}
+
+
+readonly event="\"event\":\"$1\""
+readonly hostname="\"hostname\":\"$(hostname)\""
+readonly timestamp="\"timestamp\":\"$(date -u -Ins  | tr ',' '.' | cut -c1-23)Z\""
+readonly runner_name="\"runner_name\":$(get_env_value_string RUNNER_NAME)"
+readonly repository_id="\"repository_id\":$(get_env_value_number GITHUB_REPOSITORY_ID)"
+readonly workflow_run_id="\"workflow_run_id\":$(get_env_value_number GITHUB_RUN_ID)"
+readonly workflow_run_attempt="\"workflow_run_attempt\":$(get_env_value_number GITHUB_RUN_ATTEMPT)"
 
 uuid=$(cat /proc/sys/kernel/random/uuid)
-payload="{\"event\":\"$event\",\"hostname\":\"$hostname\",\"timestamp\":\"$timestamp\"}"
+payload="{$event,$hostname,$timestamp,$runner_name,$repository_id,$workflow_run_id,$workflow_run_attempt}"
 sha256=$(printf "%s" "$payload" | sha256sum | awk '{print $1}')
 
 curl -s -k "$url" --connect-timeout 3 \
